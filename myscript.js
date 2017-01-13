d66d050272af25487a20643d6381ce9143434c92
Street Money <mutuadavid93@gmail.com>
2017-01-13 22:36:42 +0300

    Created an object using a constructor

--- myscript.js
+++ myscript.js
@@ -1,12 +1,12 @@
 
 // Traditional way
-/*function addition(a, b){
+function addition(a, b){
     return (
         console.log(a+b),
         console.info(this)
     );
 };
-addition(2, 2); */
+addition(2, 2); 
 
 // As a property of an obj
 var calc = {
@@ -20,3 +20,18 @@ var calc = {
     }  
 };
 calc.plus(10, 10);
+
+// Using a function as a constructor
+var Dog = function () {
+    var name, breed;
+};
+
+// an instance of Dog Obj
+var aDog = new Dog();
+
+// the func vars become props then we set vals
+aDog.name = "K9 Wolve";
+aDog.breed = "Poodle";
+
+// now we get the vals back
+console.log(aDog.name);
